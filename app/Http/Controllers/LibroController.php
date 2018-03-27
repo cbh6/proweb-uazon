7c436b9d19a87c5c8af843896714fb13205083d6
Cristian Botella <cri.bh6@gmail.com>
2018-03-27 11:30:54 +0200

    Register api

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class LibroController extends Controller
 {
-    //
+    public function index()
+    {
+        echo "Index de LibroController";
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        echo "register";
+        // Obtain post request
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $name = (!is_null($json) && isset($params->name)) ? $params->name : null;
+        $email = (!is_null($json) && isset($params->email)) ? $params->email : null;
+        $password = (!is_null($json) && isset($params->password)) ? $params->password : null;
+        $cp = (!is_null($json) && isset($params->cp)) ? $params->cp : null;
+        $address = (!is_null($json) && isset($params->address)) ? $params->address : null;
+        $role = 'ROLE_USER';
+
+        if (!is_null($email) && !is_null($name) && !is_null($password)) {
+
+            // Create user
+            $user = new User();
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = $password;
+            $user->cp = $cp;
+            $user->address = $address;
+            $user->role = $role;
+
+            $pwd = hash('sha256', $password);
+            $user->password = $pwd;
+
+            // Check duplicated user
+            $isset_user = User::where('email', '=', $email)->first();
+
+            if (count($isset_user) == 0) {
+                // Save user
+                $user->save();
+
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'User registered successfuly',
+                );
+            } else {
+                // Dont save user because already exists
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'User already exist, cannot register',
+                );
+            }
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'User not created',
+            );
+        }
+
+        return response()->json($data, 200);
     }
 
     public function login(Request $request)
     {
-        echo "login";
+        echo "login";die();
     }
 }
