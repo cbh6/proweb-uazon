b3d3a1d677d6e47687eb98b4385866c3fd452655
Cristian Botella <cri.bh6@gmail.com>
2018-03-28 13:29:53 +0200

    Store Libro endpoint

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\JwtAuth;
 use App\Http\Resources\AutorResource;
 use App\Http\Resources\LibroResource;
 use App\Libro;
 use Illuminate\Http\Request;
+use Validator;
 
 class LibroController extends Controller
 {
@@ -37,7 +39,61 @@ class LibroController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $hash = $request->header('Authorization', null);
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+        if ($checkToken) {
+            // Retrieve post data
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true);
+
+            // Get identified user
+            $user = $jwtAuth->checkToken($hash, true);
+
+            $validator = Validator::make($params_array, [
+                'isbn' => 'required',
+                'precio' => 'required',
+                'titulo' => 'required',
+                'editorial' => 'required',
+                'n_pags' => 'required',
+                'atributos_extra' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->messages(), 400);
+            }
+
+            // Save Libro
+            $libro = new Libro();
+            $libro->isbn = $params->isbn;
+            $libro->precio = $params->precio;
+            $libro->titulo = $params->titulo;
+            $libro->editorial = $params->editorial;
+            $libro->n_pags = $params->n_pags;
+            $libro->voto = 0;
+            $libro->num_voto = 0;
+            $libro->atributos_extra = json_encode($params->atributos_extra);
+
+            $libro->save();
+
+            $data = array(
+                'libro' => $libro,
+                'status' => 'success',
+                'message' => 'new Libro inserted successfuly',
+                'code' => 200,
+            );
+
+            return response()->json($data);
+        } else {
+            $data = array(
+                'message' => 'Not authenticated. Could not insert new Libro',
+                'status' => 'error',
+                'code' => 400,
+            );
+            return response()->json($data, 400);
+        }
+
     }
 
     /**
