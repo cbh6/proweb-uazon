5f8d29634a00b406d559353c505cc66ce2269b6c
Cristian Botella <cri.bh6@gmail.com>
2018-07-11 23:15:36 +0200

    User register page layout and styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,57 +6,60 @@
         <div class="col-md-4 auth__form-container">
             <h2 class="title auth__form-title">Login</h2>
 
-                    <form method="POST" action="{{ route('login') }}" class="auth__form">
-                        {{ csrf_field() }}
-                        
-                        <!-- email field -->
-                        <div class="row">
-                            <div class="col-md-12 auth__field">
-                                <label for="email">Email</label>
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-    
-                                @if ($errors->has('email'))
-                                    <span class="auth__errors">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <!-- password field -->
-                        <div class="row">
-                            <div class="col-md-12 auth__field">
-                                <label for="password">Password</label>
-                                <input id="password" type="password" class="form-control" name="password" required>
-    
-                                @if ($errors->has('password'))
-                                    <span class="auth__errors">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            <!-- login form -->
+            <form method="POST" action="{{ route('login') }}" class="auth__form">
+                {{ csrf_field() }}
+                
+                <!-- email field -->
+                <div class="row">
+                    <div class="col-md-12 auth__field">
+                        <label for="email">Email</label>
+                        <input class="auth__input" id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                </label>
-                            </div>
-                        </div>
+                        @if ($errors->has('email'))
+                            <span class="auth__errors">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+                
+                <!-- password field -->
+                <div class="row">
+                    <div class="col-md-12 auth__field">
+                        <label for="password">Password</label>
+                        <input class="auth__input" id="password" type="password"  name="password" required>
 
-                        <div class="row">
-                            <div class="col-md-12 auth__links">
-                                <button type="submit" class="btn auth__button">
-                                    Login
-                                </button>
+                        @if ($errors->has('password'))
+                            <span class="auth__errors">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
 
-                                <a class="auth__password-reset-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                <!-- remember me -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <label>
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
+                        </label>
+                    </div>
+                </div>
+
+                <!-- login button and reset pass link -->
+                <div class="row">
+                    <div class="col-md-12 auth__links">
+                        <button type="submit" class="btn auth__button">
+                            Login
+                        </button>
+
+                        <a class="auth__password-reset-link" href="{{ route('password.request') }}">
+                            Forgot Your Password?
+                        </a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,73 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+    <div class="row auth__row">
+        <div class="col-md-4 auth__form-container">
+            <h2 class="title auth__form-title">Registro</h2>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+            <!-- register form -->
+            <form method="POST" action="{{ route('register') }}" class="auth__form">
+                {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                <!-- nombre -->
+                <div class="row">
+                    <div class="col-md-12 auth__field">
+                        <label for="name">Nombre</label>
+                        <input class="auth__input" id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        @if ($errors->has('name'))
+                            <span class="auth__errors">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
                 </div>
-            </div>
+
+                <!-- email -->
+                <div class="row">
+                    <div class="col-md-12 auth__field">
+                        <label for="email">Email</label>
+                        <input class="auth__input" id="email" type="email" name="email" value="{{ old('email') }}" required>
+
+                        @if ($errors->has('email'))
+                            <span class="auth__errors">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <!-- password -->
+                <div class="row">
+                    <div class="col-md-12 auth__field">                        
+                        <label for="password">Contraseña</label>
+                        <input class="auth__input" id="password" type="password" name="password" required>
+
+                        @if ($errors->has('password'))
+                            <span class="auth__errors">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <!-- password confirm -->
+                <div class="row">
+                    <div class="col-md-12 auth__field">                        
+                        <label for="password-confirm">Confirmar contraseña</label>
+                        <input class="auth__input" id="password-confirm" type="password" name="password_confirmation" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">                        
+                        <button type="submit" class="btn auth__button">
+                            Registrarse
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
